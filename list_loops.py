46b305dab1963354a936be42d491db144a1617e2
lizstangle <lizstangle@me.com>
2020-09-28 12:30:41 -0700

    "finished"

--- list_loops.py
+++ list_loops.py
@@ -4,7 +4,6 @@ print(songs[1:3])
 songs[2] = "Killing Me Softly With His Song"
 songs.extend(["Fallin'", "By Your Side", "Whatever You Like"])
 del songs[0]
-# Both options will play every song in the playlist once. The first option is simpler. The second option references the length and then creates a range. 
 
 animals = ["cat", "dog", 'bird']
 animals.append("monkey")
