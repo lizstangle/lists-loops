a4960fb6efbf42f524d0e37b8b2369f0e0c5dc7c
lizstangle <lizstangle@me.com>
2020-09-28 02:13:03 -0700

    Create list_loops.py

---
+++ list_loops.py
@@ -0,0 +1,6 @@
+songs = ["ROCKSTAR", "Do It", "For The Night"]
+print(songs[0,2])
+print(songs[1:3])
+songs[2] = "Killing Me Softly With His Song"
+songs.extend(["Fallin'", "By Your Side", "Whatever You Like"])
+del songs[0]
